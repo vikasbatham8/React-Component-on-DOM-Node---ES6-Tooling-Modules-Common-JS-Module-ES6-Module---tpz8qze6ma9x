2199d37cc81a976a87290ca62e51be5f4e0f7b34
Viikas Batham <112746619+vikasbatham8@users.noreply.github.com>
2023-03-01 22:53:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
+
+
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main">
-   <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    </div>
-  )
+  return (
+    <div id="main">
+   <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
 }
 
 
